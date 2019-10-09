932c4825026b6e300cb07f02a3b1aa9f1114945c
jalejandro2928 <jalejandro2928@gmail.com>
2019-10-09 09:22:15 -0400

    asdasdasdsd

--- src/app/appPages/sample/sample/sample.component.ts
+++ src/app/appPages/sample/sample/sample.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BreadcrumbService } from 'src/app/backend/layout/breadcrumd/service/breadcrumb.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
   styleUrls: ['./sample.component.scss']
 })
-export class SampleComponent implements OnInit {
+export class SampleComponent implements OnInit, OnDestroy {
+  _unsubscribeAll: Subject<any>;
 
   constructor(private breadcrumbService: BreadcrumbService) {
+    this._unsubscribeAll = new Subject<any>();
 
   }
 
   ngOnInit() {
     this.breadcrumbService.clearBreadcrumd();
     this.breadcrumbService.setBreadcrumd('SAMPLE', true);
+  }
+
+  ngOnDestroy() {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
 }
--- src/app/appPages/sample2/sample2/sample2.component.ts
+++ src/app/appPages/sample2/sample2/sample2.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BreadcrumbService } from 'src/app/backend/layout/breadcrumd/service/breadcrumb.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sample2',
   templateUrl: './sample2.component.html',
   styleUrls: ['./sample2.component.scss']
 })
-export class Sample2Component implements OnInit {
+export class Sample2Component implements OnInit, OnDestroy {
+  _unsubscribeAll: Subject<any>;
 
-  constructor(private breadcrumbService: BreadcrumbService) { }
+  constructor(private breadcrumbService: BreadcrumbService) {
+    this._unsubscribeAll = new Subject<any>();
+  }
 
   ngOnInit() {
     this.breadcrumbService.clearBreadcrumd();
     this.breadcrumbService.setBreadcrumd('SAMPLE1', true);
+  }
+
+  ngOnDestroy() {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
 }
